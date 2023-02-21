25f1e6a240e2880d49e47d0031b4deec095e6062
aboublef <kristopherab@gmail.com>
2023-02-21 00:19:32 +0000

    add multiple rooms, hacky delete

--- internal/p2p/service.go
+++ internal/p2p/service.go
@@ -2,6 +2,8 @@ package p2p
 
 import (
 	"net/http"
+	"strconv"
+	"sync"
 
 	"tic-tac-toe/pkg/websocket"
 
@@ -25,8 +27,31 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Service) routes() {
+	var store sync.Map
+
 	s.m.Get("/", s.handleIndex())
-	s.m.Get("/ws", websocket.NewClient(2).ServeHTTP)
+	s.m.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// get search params and make sure its a number
+		// if not, return an error
+		q := r.URL.Query().Get("id")
+		if q == "" {
+			http.Error(w, "id is required", http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(q)
+		if err != nil {
+			http.Error(w, "id must be a number", http.StatusBadRequest)
+			return
+		}
+
+		client, loaded := store.LoadOrStore(id, websocket.NewClient(2))
+		if !loaded {
+			defer store.Delete(id)
+		}
+
+		client.(*websocket.Client).ServeHTTP(w, r)
+	})
 }
 
 func (s *Service) handleIndex() http.HandlerFunc {
